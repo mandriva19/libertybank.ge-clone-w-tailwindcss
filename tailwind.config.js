0283f61b2869b337c92c453e53ab82f1330b8de1
mandriva19 <97590591+mandriva19@users.noreply.github.com>
2023-08-03 17:15:34 +0400

    supper commit
    commit with tailwind setup and basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [".//**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["'BPG Arial', sans-serif;'"],
+        parimaryCaps: ["'BPG Arial Caps', sans-serif;'"]
+      }
+    },
+  },
+  plugins: [],
+}
